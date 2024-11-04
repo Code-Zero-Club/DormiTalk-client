a5dbfe7ba85397f9463a74693533b85e53589c4a
DO_S <rlaxogus7094@naver.com>
2024-11-04 10:31:10 +0900

    remove : timeFormatter

--- src/utils/timeFormatter.js
+++
@@ -1,9 +0,0 @@
-function formatTime(seconds) {
-  if (!seconds) return '00:00:00';
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const secs = Math.floor(seconds % 60);
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
-}
-
-module.exports = { formatTime };
